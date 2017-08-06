fdcf77148549883ce4e3c3a10df73f3f2bc1d842
Arek Flinik <aflinik@gmail.com>
2017-08-06 21:49:28 +0200

    Fix warnings

--- src/lib/Game.hs
+++ src/lib/Game.hs
@@ -2,11 +2,9 @@ module Game where
 
 import Definitions
 import ClientState
-import qualified Data.IntSet as IS
 import Control.Applicative ((<|>))
 
 type Scores = [Score]
---
 
 reachable :: ClaimMap -> PunterId -> SiteId -> SiteId -> Bool
 reachable cm p1 s1 s2 =
@@ -21,7 +19,7 @@ distance sm from to =
   traverseD sm [(from, 0)] to []
 
 traverseD :: SiteMap -> [(SiteId, Int)] -> SiteId -> [SiteId] -> Int
-traverseD sm [] to visited = 0
+traverseD _ [] _ _ = 0
 traverseD sm ((s,d):ss) to visited
     | s == to = d
     | otherwise = traverseD sm (ss ++ queue) to (s:visited)
@@ -41,10 +39,11 @@ scores ClientState{..} =
   where
     scoreForPunter p = sum $ map (scoreForPunterAtMine p) (siteSetToList csMines)
     scoreForPunterAtMine p m = traverseAndCollectPoints p m [m] 0 []
-    traverseAndCollectPoints p m [] score visited = score
+    traverseAndCollectPoints _ _ [] score _ = score
     traverseAndCollectPoints p m (s:ss) score visited =
         traverseAndCollectPoints p m (nextSites ++ ss) (siteScore+score) (s:visited)
       where
-        siteScore = (distance csSiteMap m s)^2
+        siteScore = d*d
+        d = distance csSiteMap m s
         nextSites = filter (\x -> not . elem x $ (ss++s:visited)) $ reachableNeighbours
         reachableNeighbours = filter (reachable csClaimMap p s) $ neighboursList csSiteMap s
