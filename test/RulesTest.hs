f46a515ef06db0bf87c462a9061cbe3374392bfb
Arek Flinik <aflinik@gmail.com>
2017-08-06 20:58:47 +0200

    Fail more tests

--- test/RulesTest.hs
+++ test/RulesTest.hs
@@ -7,6 +7,7 @@ import ClientState
 import Definitions
 import Game
 import qualified Data.IntMap.Strict as IM
+import qualified Data.IntSet as IS
 import Data.Monoid ((<>))
 
 trivialClientState :: ClientState
@@ -23,10 +24,10 @@ simpleSiteMap =
   fullSiteMap
     (map Site [0,1,2,3,4])
     [ River{rSource = 0, rTarget = 1}
-    , River{rSource = 1, rTarget = 2}
     , River{rSource = 2, rTarget = 3}
     , River{rSource = 3, rTarget = 4}
     , River{rSource = 1, rTarget = 3}
+    , River{rSource = 1, rTarget = 2}
     ]
     [1]
 
@@ -51,6 +52,17 @@ simpleClientState = ClientState
 
 unit_neighboursList =
   neighboursList simpleSiteMap 1 @?= [0,2,3]
+
+unit_siteMapSimple =
+  simpleSiteMap @?= SiteMap
+    { unSiteMap = IM.fromList
+      [ (0, SiteInfo {siNeighbours = SiteSet {unSiteSet = IS.fromList [1]}, siIsMine = False})
+      , (1, SiteInfo {siNeighbours = SiteSet {unSiteSet = IS.fromList [0,2,3]}, siIsMine = True})
+      , (2, SiteInfo {siNeighbours = SiteSet {unSiteSet = IS.fromList [1,3]}, siIsMine = False})
+      , (3, SiteInfo {siNeighbours = SiteSet {unSiteSet = IS.fromList [1,2,4]}, siIsMine = False})
+      , (4, SiteInfo {siNeighbours = SiteSet {unSiteSet = IS.fromList [3]}, siIsMine = False})
+      ]
+    }
 
 unit_scoreEmpty =
   scores emptyClientState @?= []
